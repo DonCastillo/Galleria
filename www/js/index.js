9303176d17eda38d0922cb07f65ea6c0aa3780ed
DonCastillo <don.qcastillo@yahoo.com>
2022-11-16 12:54:43 -0700

    working device ready, initial implementation of camera

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,26 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    var cameraOptions = {
+        quality: 100,
+        destinationType: Camera.DestinationType.FILE_URI, // FILE_URI
+        encodingType: Camera.EncodingType.JPEG, // JPEG
+        pictureSourceType: Camera.PictureSourceType.CAMERA, // CAMERA 
+    }
+    console.log($('#camera-button'))
+    console.log(navigator.camera)
+    $('#camera-button').on('click', takePhoto);
+
+    function takePhoto() {
+        navigator.camera.getPicture(cameraSuccess, cameraError, cameraOptions);
+    }
+
+    function cameraSuccess(image) {
+        console.log('image', image)
+    }
+
+    function cameraError(error) {
+        console.log('error', error);
+    }
 }
